1efce90d679b30b669eba0016c643fc630055835
hiepsihiep <50261825+hiepsihiep@users.noreply.github.com>
2019-05-15 18:40:19 +0700

    Add files via upload

---
+++ tim_UCLN.py
@@ -0,0 +1,10 @@
+# input so lieu
+so_thu1 = int(input("Nhap so thu 1: "))
+so_thu2 = int(input("Nhap so thu 2: "))
+
+a = so_thu1 % so_thu2
+while a != 0:
+    so_thu1 = so_thu2
+    so_thu2 = a
+    a = so_thu1 % so_thu2
+print(so_thu2)
